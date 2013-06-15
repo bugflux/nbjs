b40fd27276cd7facd50e42c1235010238e6a91a9
André Prata <bugflux@gmail.com>
2013-06-15 10:06:17 +0100

    4

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-setInterval(function() {
-	console.log('nodejs!');
-}, 2000);
+var http = require('http');
 
-console.log('Hello');
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello nodejs');
+});
+
+server.listen(8080);
