87b737c0fe613f8920881eb9f054c6664c3b03ad
André Prata <bugflux@gmail.com>
2013-06-15 10:06:37 +0100

    5

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello nodejs');
+	response.write('Hello\n');
+
+	setTimeout(function() {
+		response.end('nodejs\n');
+	}, 2000);
 });
 
 server.listen(8080);
